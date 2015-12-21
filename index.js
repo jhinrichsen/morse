855e6692014b9bcc6d2be2eb80b2ad12be7fc342
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2015-12-21 16:34:42 +0100

    feat: Add signal function
    The signal defines if there is a signal (on) or just noise (off) for a pulse.

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 // As Morse code is a digital protocol at the bottom (signal / no signal), it
 // is represented using true (signal) and false (no signal).
 
+// Look mom - no var!
+// Look dad - no for!
+
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 
@@ -62,12 +65,11 @@ const pause = ' ';
 
 // Return true if character is a long 'dah' in morse notation.
 // :: String[1] -> Boolean
-const isDah = function (c) {
-  return dah === c;
-};
+const isDah = c => { return dah === c; };
 
-expect(isDah(dah)).to.be.true;
-expect(isDah(dit)).to.be.false;
+// Avoid .to.be.true because linter will complain about expression
+expect(isDah(dah)).to.equal(true);
+expect(isDah(dit)).to.equal(false);
 
 // Return true if character is a short 'dit' in morse notation.
 // :: String[1] -> Boolean
@@ -75,8 +77,8 @@ const isDit = function (c) {
   return dit === c;
 };
 
-expect(isDit(dit)).to.be.true;
-expect(isDit(dah)).to.be.false;
+expect(isDit(dit)).to.equal(true);
+expect(isDit(dah)).to.equal(false);
 
 // 2.1 A dash is equal to three dots.
 // :: Int
@@ -96,75 +98,120 @@ const wordDotRatio = 7;
 
 // Utility: Flatten array
 // :: [a] -> [b]
-const flatten = function (as) {
-  return [].concat.apply([], as);
-};
+const flatten = as => { return [].concat.apply([], as); };
 
 // Utility: Create array of length n, and prepopulate with d
 // :: a -> Number -> [a]
-const mkArray = function (d, n) {
+const mkArray = (d, n) => {
   return Array.apply(null, new Array(n)).map(_ => d);
 };
 
 expect(mkArray(true, 2)).to.be.eql([true, true]);
 expect(mkArray(0, 3)).to.be.eql([0, 0, 0]);
 
-const mkPause = function (n) {
-  return mkArray(' ', n).join('');
-}
+const mkPause = n => { return mkArray(' ', n).join(''); };
 expect(mkPause(2)).to.equal('  ');
 
 // Encode a character text into morse.
 // :: String[1] -> String
-const encodeLetter = function (c) {
-  return table[c];
-};
+const encodeLetter = c => { return table[c]; };
 
 expect(encodeLetter('a')).to.equal('.-');
 
 // Encode a word into morse.
 // :: String -> String
-const encodeWord = function(s) {
-  // HERE const separator = mkPause(3);
-  const separator = mkPause(1);
-  return s.split('').map(encodeLetter).join(separator);
-}
+const encodeWord = s => {
+  return s
+    .split('')
+    .map(encodeLetter)
+    .join(mkPause(1));
+};
 
 expect(encodeWord('ee')).to.equal('. .');
 
 // Encode text into morse.
-// Follows ITU spec 2.4: The space between two words is equal to seven dots.
 // TODO Using a String is suboptimal, because it hardcodes the underlying buffer
 // resp. transport representation (could be file based, stream based, ...).
 // :: String -> String
-const encode = function (s) {
-  // HERE const separator = mkPause(wordDotRatio);
-  const separator = mkPause(3);
+const encode = s => {
   return s
     .toLowerCase()
     .split(' ')
     .map(encodeWord)
-    .join(separator);
-}
+    .join(mkPause(3));
+};
 
 expect(encode('morse code')).to.equal('-- --- .-. ... .   -.-. --- -.. .');
 expect(encode('FREIE ENZYKLOPAEDIE'))
     .to
     .equal('..-. .-. . .. .   . -. --.. -.-- -.- .-.. --- .--. .- . -.. .. .');
 
-// Convert morse into time notation, where short is represented as '.' and
-// a long '-' as '='.
-// Characters other than 'dit' and 'dah' are represented as '?'.
-// :: String[1] -> String[1]
-const timeNotation = function (b) {
-  return isDit(b) ? '.' : (isDah(b) ? '=' : '?');
-};
-expect(table.c
-    .split('')
-    .map(timeNotation)
-    .join(''))
-  .to.equal('=.=.');
+////////////////////////////////////////////////////////////////////////////////
+//
+// Signal functions
+//
+////////////////////////////////////////////////////////////////////////////////
 
-const s3 = '===.===...===.===.===...=.===.=...=.=.=...=' +
-  '.......===.=.===.=...===.===.===...===.=.=...=';
+// Map true -> '=' and false -> '_'
+// :: Boolean -> Character
+const wikipediaNotation = a => { return a ? '=' : '_'; };
+
+expect(wikipediaNotation([ true, true, false ])).to.eql('=', '_', '=');
+
+// Convert a text into a signal (on/ off).
+// :: String -> [Boolean]
+const signal = s => {
+  // ECMAScripts Array.prototype.join() does not offer anything other than
+  // Strings, so instead of boolean true and false the implementation is based
+  // on characters '0' and '1'.
+  const off = '0';
+  const on = '1';
+
+  // A little macro-ish like helper
+  const sep0 = n => { return mkArray(off, n).join(''); };
+  const sep1 = n => { return mkArray(on, n).join(''); };
+
+  const wordSep = sep0(wordDotRatio);
+  const letterSep = sep0(letterDotRatio);
+  const symbolSep = sep0(symbolDotRatio);
+
+  const dah = sep1(dashDotRatio);
+  const dit = sep1(1);
+  return s
+    .toLowerCase()
+    .split(' ')
+    .map(word => {
+      // console.log('* Word: ' + word);
+      return word
+        .split('')
+        .map(letter => {
+          // console.log('* * Letter: ' + letter);
+          return encodeLetter(letter)
+            .split('')
+            .map(symbol => {
+              // console.log('* * * Symbol: ' + symbol);
+              return (isDah(symbol) ? dah : dit);
+            })
+            .join(symbolSep);
+        })
+        .join(letterSep);
+    })
+    .join(wordSep)
+    .split('')
+    .map(c => { return c === on ? true : false; });
+};
+
+expect(signal('e')).to.eql([true]);
+expect(signal('ee')).to.eql([ 
+    true, 
+    false, false, false, // letter separator
+    true ]);
+
+expect(signal('morse code')
+    .map(wikipediaNotation)
+    .join(''))
+  .to
+  .equal('===_===___===_===_===___=_===_=___=_=_=___=_______' +
+      '===_=_===_=___===_===_===___===_=_=___=');
+
 // EOF
